a768636e26b9049e25895c1bf8efcd45f606e2e3
Shane Richards <shane.richards121@gmail.com>
2017-08-07 14:06:41 -0500

    feat: menu drop down item

--- app/src/main/java/com/shane/popularmovies/activities/MainActivity.java
+++ app/src/main/java/com/shane/popularmovies/activities/MainActivity.java
@@ -3,8 +3,13 @@ package com.shane.popularmovies.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.shane.popularmovies.R;
 import com.shane.popularmovies.adapters.MovieAdapter;
@@ -36,5 +41,22 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
         final Intent intent = new Intent(MainActivity.this, MovieDetailActivity.class);
         intent.putExtra(Constants.EXTRA_MOVIE, movie);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        MenuItem item = menu.findItem(R.id.action_sort);
+        Spinner sortOptionsSpinner = (Spinner) MenuItemCompat.getActionView(item);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter
+                .createFromResource(this, R.array.sort_order_array, R.layout.item_sort_option);
+
+        adapter.setDropDownViewResource(R.layout.item_sort_by_dropdown);
+
+        sortOptionsSpinner.setAdapter(adapter);
+
+        return true;
     }
 }
--- app/src/main/java/com/shane/popularmovies/adapters/MovieAdapter.java
+++ app/src/main/java/com/shane/popularmovies/adapters/MovieAdapter.java
@@ -57,7 +57,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
     public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater
                 .from(parent.getContext())
-                .inflate(R.layout.movie_list_item, parent, false);
+                .inflate(R.layout.item_movie_list, parent, false);
 
         return new MovieViewHolder(view);
     }
