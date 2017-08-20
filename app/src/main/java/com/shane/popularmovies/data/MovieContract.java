5253d5f3a1ff5c8d4496c15ae065af0d9087dc9a
Shane Richards <shane.richards121@gmail.com>
2017-08-20 17:27:45 -0500

    refactor: prevent instantion of MovieContract

--- app/src/main/java/com/shane/popularmovies/data/MovieContract.java
+++ app/src/main/java/com/shane/popularmovies/data/MovieContract.java
@@ -6,13 +6,16 @@ import android.provider.BaseColumns;
  * Created by Shane on 8/17/2017.
  */
 
-public class MovieContract {
+public final class MovieContract {
+
+    private MovieContract() {
+        throw new AssertionError("Cannot instantiate MovieContract");
+    }
 
     public static final class MovieEntry implements BaseColumns {
         public static final String TABLE_NAME = "movie";
         public static final String COLUMN_ID = "id";
         public static final String COLUMN_TITLE = "title";
         public static final String COLUMN_OVERVIEW = "overview";
-
     }
 }
